6d1f029894bb8a62395748464f002047bce69060
Nicolas Duarte <nicolas_duarterj@proton.me>
2025-05-30 20:55:44 -0300

    Terminar inicialização do banco

--- src/main/java/com/nduarte/rpgmantrue/database/MainSQLiteConnection.java
+++ src/main/java/com/nduarte/rpgmantrue/database/MainSQLiteConnection.java
@@ -62,6 +62,105 @@ public abstract class MainSQLiteConnection {
             ee.initCause(e);
             throw ee;
         }
+        
+        try {
+            String sqlEquipamentos = "CREATE TABLE IF NOT EXISTS Equipamentos("
+                    + "Id INTEGER PRIMARY KEY AUTOINCREMENT,"
+                    + "Nome VARCHAR(50) NOT NULL,"
+                    + "Dono INTEGER NOT NULL,"
+                    + "EstaEquipado BOOLEAN NOT NULL,"
+                    + "FOREIGN KEY(Dono) REFERENCES Personagens(Id));";
+            
+            Statement stmtEquipamentos = dbConn.createStatement();
+            stmtEquipamentos.execute(sqlEquipamentos);
+        } catch(SQLException e) {
+            var ee = new DatabaseInitializationException("Failed to init table ItensConsumiveis");
+            ee.initCause(e);
+            throw ee;
+        }
+        
+        try {
+            String sqlMaxUsosMagia = "CREATE TABLE IF NOT EXISTS MaxUsosMagia("
+                    + "Id_Personagem INTEGER PRIMARY KEY,"
+                    + "Nivel1 INTEGER,"
+                    + "Nivel2 INTEGER,"
+                    + "Nivel3 INTEGER,"
+                    + "Nivel4 INTEGER,"
+                    + "Nivel5 INTEGER,"
+                    + "Nivel6 INTEGER,"
+                    + "Nivel7 INTEGER,"
+                    + "Nivel8 INTEGER,"
+                    + "Nivel9 INTEGER,"
+                    + "FOREIGN KEY(Id_Personagem) REFERENCES Personagens(Id));";
+            
+            Statement stmtMaxUsosMagia = dbConn.createStatement();
+            stmtMaxUsosMagia.execute(sqlMaxUsosMagia);
+        } catch(SQLException e) {
+            var ee = new DatabaseInitializationException("Failed to init MaxUsosMagia");
+            ee.initCause(e);
+            throw ee;
+        }
+        
+        try {
+            String sqlUsosRestantesMagia = "CREATE TABLE IF NOT EXISTS UsosRestantesMagia("
+                    + "Id_Personagem INTEGER PRIMARY KEY,"
+                    + "Nivel1 INTEGER,"
+                    + "Nivel2 INTEGER,"
+                    + "Nivel3 INTEGER,"
+                    + "Nivel4 INTEGER,"
+                    + "Nivel5 INTEGER,"
+                    + "Nivel6 INTEGER,"
+                    + "Nivel7 INTEGER,"
+                    + "Nivel8 INTEGER,"
+                    + "Nivel9 INTEGER,"
+                    + "FOREIGN KEY(Id_Personagem) REFERENCES Personagens(Id));";
+            
+            Statement stmtUsosRestantes = dbConn.createStatement();
+            stmtUsosRestantes.execute(sqlUsosRestantesMagia);
+        } catch(SQLException e) {
+            var ee = new DatabaseInitializationException("Failed to init UsosRestantes");
+            ee.initCause(e);
+            throw ee;
+        }
+        
+        try {
+            String sqlAtaques = "CREATE TABLE IF NOT EXISTS Ataques("
+                    + "Id INTEGER PRIMARY KEY AUTOINCREMENT,"
+                    + "Nome VARCHAR(50) NOT NULL,"
+                    + "Dono INTEGER NOT NULL,"
+                    + "NivelMagico INTEGER,"
+                    + "DependEquipId INTEGER,"
+                    + "TipoDado INTEGER NOT NULL,"
+                    + "NumeroDado INTEGER NOT NULL,"
+                    + "Bonus INTEGER DEFAULT 0,"
+                    + "FOREIGN KEY(Dono) REFERENCES Personagens(Id),"
+                    + "FOREIGN KEY(DependEquipId) REFERENCES Equipamentos(Id),"
+                    + "UNIQUE(Dono,Nome));";
+            
+            Statement stmtAtaques = dbConn.createStatement();
+            stmtAtaques.execute(sqlAtaques);
+        } catch(SQLException e) {
+            var ee = new DatabaseInitializationException("Failed to init Ataques");
+            ee.initCause(e);
+            throw ee;
+        }
+        
+        try {
+            String sqlConsumos = "CREATE TABLE IF NOT EXISTS AtaquesConsumos("
+                    + "Id_Ataque INTEGER NOT NULL,"
+                    + "Id_Item INTEGER NOT NULL,"
+                    + "Quantidade REAL NOT NULL,"
+                    + "PRIMARY KEY(Id_Ataque,Id_Item),"
+                    + "FOREIGN KEY(Id_Ataque) REFERENCES Ataques(Id),"
+                    + "FOREIGN KEY(Id_Item) REFERENCES ItensConsumiveis(Id));";
+            
+            Statement stmtConsumos = dbConn.createStatement();
+            stmtConsumos.execute(sqlConsumos);
+        } catch (SQLException e) {
+            var ee = new DatabaseInitializationException("Failed to init AtaquesConsumos");
+            ee.initCause(e);
+            throw ee;
+        }
     }
     
     public static Connection getConn() {
