3aef42a586e79c7831402b1c1026a1f68f929c47
Nicolas Duarte <nicolas_duarterj@proton.me>
2025-06-13 04:33:46 -0300

    Adicionar verificação de número de personagens pré seleção

--- src/main/java/com/nduarte/rpgmantrue/controllers/MainMenuController.java
+++ src/main/java/com/nduarte/rpgmantrue/controllers/MainMenuController.java
@@ -9,7 +9,9 @@ import java.util.ResourceBundle;
 import javafx.fxml.Initializable;
 import javafx.fxml.FXML;
 import com.nduarte.rpgmantrue.App;
+import com.nduarte.rpgmantrue.models.Personagem;
 import java.io.IOException;
+import javafx.scene.control.Alert;
 
 /**
  * FXML Controller class
@@ -33,6 +35,13 @@ public class MainMenuController implements Initializable {
     
     @FXML
     public void selectChar() throws IOException {
+        if (Personagem.getAllChars().isEmpty()) {
+            Alert emptyCharListWarning = new Alert(Alert.AlertType.ERROR);
+            emptyCharListWarning.setHeaderText("Nenhum personagem encontrado.");
+            emptyCharListWarning.setContentText("Crie um personagem antes de utilizar esta função.");
+            emptyCharListWarning.showAndWait();
+            return;
+        }
         App.setRoot("CharSelect");
     }
     
