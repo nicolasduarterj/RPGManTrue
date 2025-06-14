ae7c6771ff912a987ef88d93b9ac0663b5f5b475
Nicolas Duarte <nicolas_duarterj@proton.me>
2025-06-14 20:21:21 -0300

    Adicionar interação na tela CharMagicMax

--- src/main/java/com/nduarte/rpgmantrue/controllers/CharMagicMaxController.java
+++ src/main/java/com/nduarte/rpgmantrue/controllers/CharMagicMaxController.java
@@ -4,10 +4,80 @@
  */
 package com.nduarte.rpgmantrue.controllers;
 
+import com.nduarte.rpgmantrue.models.SelectedCharacter;
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.TextField;
+
 /**
  *
  * @author nduarte
  */
 public class CharMagicMaxController extends CharManagementController {
     
+    @FXML
+    TextField nivel1Label;
+    
+    @FXML
+    TextField nivel2Label;
+    
+    @FXML
+    TextField nivel3Label;
+    
+    @FXML
+    TextField nivel4Label;
+    
+    @FXML
+    TextField nivel5Label;
+    
+    @FXML
+    TextField nivel6Label;
+    
+    @FXML
+    TextField nivel7Label;
+    
+    @FXML
+    TextField nivel8Label;
+    
+    @FXML
+    TextField nivel9Label;
+    
+    @FXML
+    public void initialize() {
+        nivel1Label.setText(String.valueOf(SelectedCharacter.get().getInfoMagica().getMaxUsosPorNivel(1)));
+        nivel2Label.setText(String.valueOf(SelectedCharacter.get().getInfoMagica().getMaxUsosPorNivel(2)));
+        nivel3Label.setText(String.valueOf(SelectedCharacter.get().getInfoMagica().getMaxUsosPorNivel(3)));
+        nivel4Label.setText(String.valueOf(SelectedCharacter.get().getInfoMagica().getMaxUsosPorNivel(4)));
+        nivel5Label.setText(String.valueOf(SelectedCharacter.get().getInfoMagica().getMaxUsosPorNivel(5)));
+        nivel6Label.setText(String.valueOf(SelectedCharacter.get().getInfoMagica().getMaxUsosPorNivel(6)));
+        nivel7Label.setText(String.valueOf(SelectedCharacter.get().getInfoMagica().getMaxUsosPorNivel(7)));
+        nivel8Label.setText(String.valueOf(SelectedCharacter.get().getInfoMagica().getMaxUsosPorNivel(8)));
+        nivel9Label.setText(String.valueOf(SelectedCharacter.get().getInfoMagica().getMaxUsosPorNivel(9)));
+    }
+    
+    public void saveMaxStats() {
+        try {
+            SelectedCharacter.get().getInfoMagica().setMaxUsosPorNivel(1, Integer.parseInt(nivel1Label.getText()));
+            SelectedCharacter.get().getInfoMagica().setMaxUsosPorNivel(2, Integer.parseInt(nivel2Label.getText()));
+            SelectedCharacter.get().getInfoMagica().setMaxUsosPorNivel(3, Integer.parseInt(nivel3Label.getText()));
+            SelectedCharacter.get().getInfoMagica().setMaxUsosPorNivel(4, Integer.parseInt(nivel4Label.getText()));
+            SelectedCharacter.get().getInfoMagica().setMaxUsosPorNivel(5, Integer.parseInt(nivel5Label.getText()));
+            SelectedCharacter.get().getInfoMagica().setMaxUsosPorNivel(6, Integer.parseInt(nivel6Label.getText()));
+            SelectedCharacter.get().getInfoMagica().setMaxUsosPorNivel(7, Integer.parseInt(nivel7Label.getText()));
+            SelectedCharacter.get().getInfoMagica().setMaxUsosPorNivel(8, Integer.parseInt(nivel8Label.getText()));
+            SelectedCharacter.get().getInfoMagica().setMaxUsosPorNivel(9, Integer.parseInt(nivel9Label.getText()));
+            
+            SelectedCharacter.get().getInfoMagica().saveMaxStats();
+            
+            Alert ok = new Alert(Alert.AlertType.INFORMATION);
+            ok.setHeaderText("Salvo!");
+            ok.setContentText("Informações mágicas salvas!");
+            ok.showAndWait();
+        } catch (Exception e) {
+            Alert numErr = new Alert(Alert.AlertType.ERROR);
+            numErr.setHeaderText("Erro no salvamento!");
+            numErr.setContentText(e.getMessage());
+            numErr.showAndWait();
+        }       
+    }
 }
--- src/main/java/com/nduarte/rpgmantrue/models/InfoMagia.java
+++ src/main/java/com/nduarte/rpgmantrue/models/InfoMagia.java
@@ -93,5 +93,59 @@ public class InfoMagia {
         return new InfoMagia(maxUsos, currentUsos, charId);
     }
     
-
+    //-----------------------[Getters e setters]------------------------//
+    
+    public int getMaxUsosPorNivel(int nivel) {
+        if (nivel < 1 || nivel > 9)
+            return 0;
+        
+        return magiasMax[nivel - 1];
+    }
+    
+    public int getUsosRestantesPorNivel(int nivel) {
+        if (nivel < 1 || nivel > 9) {
+            return 0;
+        }
+        
+        return magiasAtuais[nivel - 1];
+    }
+    
+    public void setMaxUsosPorNivel(int nivel, int x) throws IllegalArgumentException {
+        if (nivel < 1 || nivel > 9) throw new IllegalArgumentException("Niveis são de 1 a 9.");
+        if (x < 0) throw new IllegalArgumentException("Valores máximos de magia devem ser positivos.");
+        
+        magiasMax[nivel - 1] = x;
+    }
+    
+    public void gastarUsoRestantesPorNivel(int nivel) throws IllegalArgumentException {
+        if (nivel < 1 || nivel > 9) throw new IllegalArgumentException("Niveis são de 1 a 9.");
+        if (magiasAtuais[nivel - 1] == 0) throw new IllegalArgumentException("Não há usos disponíveis para gastar");
+        
+        magiasAtuais[nivel - 1]--;
+    }
+    
+    public void saveMaxStats() {
+        String sql = "INSERT OR REPLACE INTO MaxUsosMagia("
+                + "Id_Personagem,"
+                + "Nivel1,"
+                + "Nivel2,"
+                + "Nivel3,"
+                + "Nivel4,"
+                + "Nivel5,"
+                + "Nivel6,"
+                + "Nivel7,"
+                + "Nivel8,"
+                + "Nivel9) VALUES (?,?,?,?,?,?,?,?,?,?);";
+        
+        try {
+            PreparedStatement stmt = MainSQLiteConnection.getConn().prepareStatement(sql);
+            stmt.setInt(1, donoId);
+            for (int i = 2; i <= 10; i++) {
+                stmt.setInt(i, getMaxUsosPorNivel(i - 1));
+            }
+            stmt.execute();
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
 }
